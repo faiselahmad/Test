d832749cfd0ed36cff857aea77b3a1b1dfa99fcb
Faisel <Faisel@DESKTOP-UBVLE2S>
2022-06-22 13:46:43 +0530

    Updates

--- abc/src/abc/dc.java
+++ abc/src/abc/dc.java
@@ -4,7 +4,8 @@ public class dc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello..");
+		String a="ksshd";
+		System.out.print(a);
 	}
 
 }
